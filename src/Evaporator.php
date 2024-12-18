df449575b31d47ea9cb661a42aa2e74f63fe8446
Chris Yates <beastbytes@users.noreply.github.com>
2024-12-18 18:15:00 +0000

    Create Evaporator.php

---
+++ src/Evaporator.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BeastBytes\Evaporator;
+
+use AllowDynamicProperties;
+use App\Evaporator\AttributeHandling\ColumnAttributesHandler;
+use ReflectionClass;
+use Yiisoft\Hydrator\AttributeHandling\ResolverFactory\ReflectionAttributeResolverFactory;
+
+#[AllowDynamicProperties]
+final class Evaporator implements EvaporatorInterface
+{
+    private ColumnAttributesHandler $columnAttributesHandler;
+
+    public function __construct(
+    )
+    {
+        $attributeResolverFactory ??= new ReflectionAttributeResolverFactory();
+        $this->columnAttributesHandler = new ColumnAttributesHandler($attributeResolverFactory, $this);
+    }
+
+    public function evaporate(object $object, array $properties): array
+    {
+        $columns = [];
+
+        $reflectionClass = new ReflectionClass($object);
+        $reflectionProperties = $reflectionClass->getProperties();
+
+        foreach ($reflectionProperties as $property) {
+            $propertyName = $property->getName();
+            if (array_key_exists($propertyName, $properties)) {
+                $result = $this->columnAttributesHandler->handle($object, $property);
+                if ($result->isResolved()) {
+                    $columns[$properties[$propertyName]] = $result->getValue();
+                }
+            }
+        }
+
+        return $columns;
+    }
+}
